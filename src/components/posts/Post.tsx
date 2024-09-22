ec5eb03b5b642ca15ab64b5e4cee2811b5385e64
Yusup Rejebov <yusuprd@icloud.com>
2024-09-22 23:21:28 +0800

    Added styling on post published and time published logic

---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,37 @@
+import { PostData } from "@/lib/types";
+import Link from "next/link";
+import UserProfile from "../UserProfile";
+import { formatRelativeDate } from "@/lib/utils";
+
+interface PostProps {
+	post: PostData;
+}
+
+export default function Post({ post }: PostProps) {
+	return (
+		<article className="space-y-3 rounded-lg bg-card p-5 shadow-sm">
+			<div className="flex flex-wrap gap-3">
+				<Link href={`/users/${post.user.username}`}>
+					<UserProfile avatarUrl={post.user.avatarUrl} />
+				</Link>
+				<div>
+					<Link
+						href={`/users/${post.user.username}`}
+						className="block font-medium hover:underline"
+					>
+						{post.user.displayName}
+					</Link>
+					<Link
+						href={`/posts/${post.id}`}
+						className="block text-sm text-muted-foreground hover:underline"
+					>
+						{formatRelativeDate(post.createdAt)}
+					</Link>
+				</div>
+			</div>
+			<div className="whitespace-pre-line break-words">
+				{post.content}
+			</div>
+		</article>
+	);
+}
