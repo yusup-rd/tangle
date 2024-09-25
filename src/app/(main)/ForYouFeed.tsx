752dd8c76c6e08902fda3c7a06c8eedfbb7c6d9a
Yusup Rejebov <yusuprd@icloud.com>
2024-09-25 17:25:33 +0800

    Added pending skeleton

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -2,6 +2,7 @@
 
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 import kyInstance from "@/lib/ky";
 import { PostPage } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -31,7 +32,15 @@ export default function ForYouFeed() {
 	const posts = data?.pages.flatMap((page) => page.posts) || [];
 
 	if (status === "pending") {
-		return <Loader2 className="mx-auto animate-spin" />;
+		return <PostsLoadingSkeleton />;
+	}
+
+	if (status === "success" && !posts.length && !hasNextPage) {
+		return (
+			<p className="text-center text-muted-foreground">
+				There are no posts in here.
+			</p>
+		);
 	}
 
 	if (status === "error") {
@@ -52,7 +61,9 @@ export default function ForYouFeed() {
 			{posts.map((post) => (
 				<Post key={post.id} post={post} />
 			))}
-			{isFetchingNextPage &&  <Loader2 className="mx-auto my-3 animate-spin" />}
+			{isFetchingNextPage && (
+				<Loader2 className="mx-auto my-3 animate-spin" />
+			)}
 		</InfiniteScrollContainer>
 	);
 }
