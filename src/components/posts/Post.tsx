2e9f90a17906a855f554895311cf9fcb1ec6a8ad
Yusup <yusuprd@icloud.com>
2024-10-03 22:03:39 +0800

    Added delete button

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,33 +1,47 @@
+"use client";
+
 import { PostData } from "@/lib/types";
 import Link from "next/link";
 import UserProfile from "../UserProfile";
 import { formatRelativeDate } from "@/lib/utils";
+import { useSession } from "@/app/(main)/SessionProvider";
+import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
 	post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
+	const { user } = useSession();
+
 	return (
-		<article className="space-y-3 rounded-lg bg-card p-5 shadow-sm">
-			<div className="flex flex-wrap gap-3">
-				<Link href={`/users/${post.user.username}`}>
-					<UserProfile avatarUrl={post.user.avatarUrl} />
-				</Link>
-				<div>
-					<Link
-						href={`/users/${post.user.username}`}
-						className="block font-medium hover:underline"
-					>
-						{post.user.displayName}
+		<article className="group/post space-y-3 rounded-lg bg-card p-5 shadow-sm">
+			<div className="flex justify-between gap-3">
+				<div className="flex flex-wrap gap-3">
+					<Link href={`/users/${post.user.username}`}>
+						<UserProfile avatarUrl={post.user.avatarUrl} />
 					</Link>
-					<Link
-						href={`/posts/${post.id}`}
-						className="block text-sm text-muted-foreground hover:underline"
-					>
-						{formatRelativeDate(post.createdAt)}
-					</Link>
+					<div>
+						<Link
+							href={`/users/${post.user.username}`}
+							className="block font-medium hover:underline"
+						>
+							{post.user.displayName}
+						</Link>
+						<Link
+							href={`/posts/${post.id}`}
+							className="block text-sm text-muted-foreground hover:underline"
+						>
+							{formatRelativeDate(post.createdAt)}
+						</Link>
+					</div>
 				</div>
+				{post.user.id === user.id && (
+					<PostMoreButton
+						post={post}
+						className="opacity-0 transition-opacity group-hover/post:opacity-100"
+					/>
+				)}
 			</div>
 			<div className="whitespace-pre-line break-words">
 				{post.content}
---
+++ src/components/posts/mutations.ts
@@ -0,0 +1,60 @@
+import { useToast } from "../ui/use-toast";
+import {
+	InfiniteData,
+	QueryFilters,
+	useMutation,
+	useQueryClient,
+} from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
+import { deletePost } from "./actions";
+import { PostPage } from "@/lib/types";
+
+export function useDeletePostMutation() {
+	const { toast } = useToast();
+	const queryClient = useQueryClient();
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const mutation = useMutation({
+		mutationFn: deletePost,
+		onSuccess: async (deletedPost) => {
+			const queryFilter: QueryFilters = { queryKey: ["post-feed"] };
+
+			await queryClient.cancelQueries(queryFilter);
+
+			queryClient.setQueriesData<InfiniteData<PostPage, string | null>>(
+				queryFilter,
+				(oldData) => {
+					if (!oldData) return;
+
+					return {
+						pageParams: oldData.pageParams,
+						pages: oldData.pages.map((page) => ({
+							nextCursor: page.nextCursor,
+							posts: page.posts.filter(
+								(p) => p.id !== deletedPost.id,
+							),
+						})),
+					};
+				},
+			);
+
+			toast({
+				description: "Post deleted",
+			});
+
+			if (pathname === `/post/${deletedPost.id}`) {
+				router.push(`/users/${deletedPost.user.username}`);
+			}
+		},
+		onError(error) {
+			console.error(error);
+			toast({
+				variant: "destructive",
+				description: "Failed to delete post. Please try again.",
+			});
+		},
+	});
+
+	return mutation;
+}
