5ac94854333f7bb8ce418f0904e18352b5a59228
Yusup <yusuprd@icloud.com>
2024-10-08 19:06:40 +0800

    Added Following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,26 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import TrendsSidebar from "@/components/TrendsSidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ForYouFeed from "./ForYouFeed";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
 	return (
 		<main className="flex w-full min-w-0 gap-5">
 			<div className="w-full min-w-0 space-y-5">
 				<PostEditor />
-				<ForYouFeed />
+				<Tabs defaultValue="for-you">
+					<TabsList>
+						<TabsTrigger value="for-you">For you</TabsTrigger>
+						<TabsTrigger value="following">Following</TabsTrigger>
+					</TabsList>
+					<TabsContent value="for-you">
+						<ForYouFeed />
+					</TabsContent>
+					<TabsContent value="following">
+						<FollowingFeed />
+					</TabsContent>
+				</Tabs>
 			</div>
 			<TrendsSidebar />
 		</main>
--- src/app/api/posts/following/route.ts
+++ src/app/api/posts/following/route.ts
@@ -14,10 +14,19 @@ export async function GET(req: NextRequest) {
 		}
 
 		const posts = await prisma.post.findMany({
-			include: getPostDataInclude(user.id),
+			where: {
+				user: {
+					followers: {
+						some: {
+							followerId: user.id,
+						},
+					},
+				},
+			},
 			orderBy: { createdAt: "desc" },
 			take: pageSize + 1,
 			cursor: cursor ? { id: cursor } : undefined,
+			include: getPostDataInclude(user.id),
 		});
 
 		const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
@@ -31,7 +40,7 @@ export async function GET(req: NextRequest) {
 	} catch (error) {
 		console.error(error);
 		
-		return Response.json(
+        return Response.json(
 			{ error: "Internal server error" },
 			{ status: 500 },
 		);
