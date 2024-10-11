8ba1ca2f57fb1b37d07629990596cc7c2e466e63
Yusup <yusuprd@icloud.com>
2024-10-11 19:49:21 +0800

    Added and tested cron job for deleting unused media

---
+++ src/app/api/clear-uploads/route.ts
@@ -0,0 +1,54 @@
+import prisma from "@/lib/prisma";
+import { UTApi } from "uploadthing/server";
+
+export async function GET(req: Request) {
+	try {
+		const authHeader = req.headers.get("Authorization");
+
+		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+			return Response.json({ error: "Unauthorized" }, { status: 401 });
+		}
+
+		const unusedMedia = await prisma.media.findMany({
+			where: {
+				postId: null,
+				...(process.env.NODE_ENV === "production"
+					? {
+							createdAt: {
+								lte: new Date(Date.now() - 1000 * 60 * 60 * 24),
+							},
+						}
+					: {}),
+			},
+			select: {
+				id: true,
+				url: true,
+			},
+		});
+
+		new UTApi().deleteFiles(
+			unusedMedia.map(
+				(media) =>
+					media.url.split(
+						`/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+					)[1],
+			),
+		);
+
+		await prisma.media.deleteMany({
+			where: {
+				id: {
+					in: unusedMedia.map((media) => media.id),
+				},
+			},
+		});
+
+		return new Response();
+	} catch (error) {
+		console.error(error);
+		return Response.json(
+			{ error: "Internal Server Error" },
+			{ status: 500 },
+		);
+	}
+}
