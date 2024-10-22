4fec54430b16c48f5bacf1fac60be7612940f8dc
Yusup <yusuprd@icloud.com>
2024-10-22 22:19:34 +0800

    Fix design on smaller screen

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
 	return (
 		<header className="sticky top-0 z-10 bg-card shadow-sm">
-			<div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+			<div className="mx-auto flex max-w-7xl items-center justify-center gap-5 px-5 py-3">
 				<Link href="/" className="text-2xl font-bold text-primary">
 					Tangle
 				</Link>
